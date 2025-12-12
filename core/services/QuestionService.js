f47ac4af283b25653b6a13d63b476a3349cda1f4
mohammedd2002 <168116874+mohammedd2002@users.noreply.github.com>
2025-12-12 16:08:51 +0200

    Feat(Logic): Service for Choice , Question and Student

---
+++ core/services/QuestionService.js
@@ -0,0 +1,33 @@
+class QuestionService extends StorageService {
+
+    store(question = {}) {
+
+        let questions = this.get();
+        questions.push(question);
+        this.setItem('questions', questions);
+    }
+
+    get() {
+
+        return this.getItem('questions');
+    }
+
+    getById(questionId) {
+
+        let questions = this.get();
+        return questions.find(function (question) {
+            return question.id == questionId;
+
+        });
+    }
+
+    getByExam(examId) {
+
+        let questions = this.get();
+        return questions.filter(function (question) {
+
+            return question.examId == examId;
+        });
+    }
+}
+
